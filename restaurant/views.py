da00e462488291cc5eebff0cc9cfb8978884c2ed
Anthony <tonyudeagbala@gmail.com>
2022-03-04 18:50:24 +0000

    Serialize data

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,4 @@
+from itsdangerous import serializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -13,3 +14,6 @@ def api_menu_detail_view(request, id):
         menu_item = Menu.objects.get(id=id)
     except Menu.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = MenuSerializer(menu_item)
+    return Response(serializer.data)
