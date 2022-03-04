9c1429e2822fdf17a2ec8511415f146a07adfbb4
Anthony <tonyudeagbala@gmail.com>
2022-03-04 18:53:40 +0000

    Add generic comments

--- restaurant/views.py
+++ restaurant/views.py
@@ -9,11 +9,20 @@ from .serializers import MenuSerializer
 
 @api_view(['GET',])
 def api_menu_detail_view(request, id):
-    
+    """
+        An endpoint to get the detail of a menu item
+
+        variables:
+                - menu_item = stores the item gotten through url id parameter
+                - serializer = stors the serialized data
+    """
+
+    #   Check if item id exists using try block
     try:
         menu_item = Menu.objects.get(id=id)
     except Menu.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
+    #   Save and return serialized data
     serializer = MenuSerializer(menu_item)
     return Response(serializer.data)
