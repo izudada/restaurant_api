8888521b108ceec08cadd610ff68b81c00fbfbe3
Anthony <tonyudeagbala@gmail.com>
2022-03-04 18:31:43 +0000

    Rename serializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Menu
 
-class RestaurantSerializer(serializers.ModelSerializer):
+class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = ['id', 'restaurant', 'dish', 'description', 'image_url', 'created_at']
