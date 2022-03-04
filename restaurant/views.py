6ce9b16e8c1c077a4ffcb6b975aa19519cf91e9c
Anthony <tonyudeagbala@gmail.com>
2022-03-04 18:30:47 +0000

    Import Restaurant serializer

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,4 +3,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from .models import Menu, Restaurant
+from .serializers import RestaurantSerializer
+
 
