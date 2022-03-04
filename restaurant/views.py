8d41e60ffeb983df9ccc0c1ebc6a58c1abd45ad0
Anthony <tonyudeagbala@gmail.com>
2022-03-04 20:48:51 +0000

    api_menu_update_view

--- restaurant/views.py
+++ restaurant/views.py
@@ -25,3 +25,26 @@ def api_menu_detail_view(request, id):
     #   Save and return serialized data
     serializer = MenuSerializer(menu_item)
     return Response(serializer.data)
+
+@api_view(['PUT',])
+def api_menu_update_view(request, id):
+    """
+
+    """
+
+    #   Check if item id exists using try block
+    try:
+        menu_item = Menu.objects.get(id=id)
+    except Menu.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    #   Save and return serialized data
+    serializer = MenuSerializer(menu_item, data=request.data)
+    data = {}
+
+    if serializer.is_valid():
+        serializer.save()
+        data["success"] = "update successful"
+        return Response(data=data)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
