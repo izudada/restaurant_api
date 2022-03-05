cd71e199ea160d104070d6eebcee5c8640e54a2a
Anthony <tonyudeagbala@gmail.com>
2022-03-05 11:51:24 +0000

    Fix 201 status bug

--- restaurant/views.py
+++ restaurant/views.py
@@ -103,5 +103,5 @@ def api_menu_create_view(request):
     #   Check if serializer is valid
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATE)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
